218a9610f3a5c557901e17d8c57ce5fff630c681
Marcin Puka <marcinpuka@gmail.com>
2017-09-15 18:55:30 +0200

    Add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+var _ = require("lodash");
 var express = require("express");
 var bodyParser = require("body-parser");
 var {ObjectID} = require("mongodb");
@@ -70,6 +71,38 @@ app.delete("/todos/:id", (req, res) => {
         res.status(400).send();
     });
 });
+
+app.patch("/todos/:id", (req, res) => {
+   var id = req.params.id;
+   var body = _.pick(req.body, ["text", "completed"]);    
+    
+   if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+   }
+    
+    if (_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    } else {
+     body.completed = false;
+     body.completed = null;    
+    }
+    
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+        if(!todo) {
+            return res.status(404).send();
+        }
+        
+        res.send({todo});
+    }).catch((e) => {
+        res.status(400).send();
+    });
+});
+
+
+
+
+
+
 
 //var newUser = new User({
 //   email: "johndoe@gmail.com" 
