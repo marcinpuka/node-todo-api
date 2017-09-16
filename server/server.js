b79822acf8832fb15dfa0d76602fbe00b5a515b9
Marcin Puka <marcinpuka@gmail.com>
2017-09-16 15:55:36 +0200

    Setup separate test and development envs

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+require("./config/config");
+
 var _ = require("lodash");
 var express = require("express");
 var bodyParser = require("body-parser");
@@ -8,7 +10,7 @@ var {Todo} = require("./models/todo");
 var {User} = require("./models/user");
 
 var app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 
 app.use(bodyParser.json());
@@ -136,7 +138,7 @@ app.patch("/todos/:id", (req, res) => {
 //});
 
 app.listen(port, () => {
-   console.log(`Starte up at port ${port}`); 
+   console.log(`Started up at port ${port}`); 
 });
 
 module.exports = {app};
